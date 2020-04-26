eaba1bf678d508647304ce9cb9b2c99f69f811c5
dj8639rsm <dj8639rsm@gmail.com>
2020-04-26 22:10:48 +0900

    bindingbase 実装

--- xamarinApp1/xamarinApp1/MainPage.xaml.cs
+++ xamarinApp1/xamarinApp1/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +13,49 @@ namespace xamarinApp1
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
+    public class BindableBase:INotifyPropertyChanged
+    {
+
+        protected BindableBase()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool SerProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if(object.Equals(field, value)) { return false; }
+
+            field = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+    public class MyPageViewModel:BindableBase
+    {
+        private string message;
+        //ローカル変数の定義
+
+        public string Message
+        {
+            get { return this.message; }
+            set { this.SerProperty(ref this.message, value); }
+        }
+        
+        public Command NowCommand { get; }
+
+        public MyPageViewModel()
+        {
+            this.NowCommand = new Command(_ => this.Message = DateTime.Now.ToString());
+        }
+    }
     
     public partial class MainPage : ContentPage
     {
@@ -20,10 +64,7 @@ namespace xamarinApp1
             InitializeComponent();   
         }
 
-        private void ButtonLabelChenge_Clicked(object sender, EventArgs e)
-        {
-            this.label.Text = DateTime.Now.ToString();
-        }
+        
     }
 }
 
