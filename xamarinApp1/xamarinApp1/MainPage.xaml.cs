7c5310fa63c2756c4d1a9eb41bf0fbc730a4767c
dj8639rsm <dj8639rsm@gmail.com>
2020-04-26 15:44:04 +0900

    scroll view 実装

--- xamarinApp1/xamarinApp1/MainPage.xaml.cs
+++ xamarinApp1/xamarinApp1/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,61 +12,18 @@ namespace xamarinApp1
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-
-    public class Person : BindableObject
-    {
-        public static readonly BindableProperty NameProperty = BindableProperty.Create(
-            "name",
-            typeof(string),
-            typeof(Person),
-            "tanaka",
-            propertyChanged:OnNameChanged,
-            validateValue:OnNameValidationValue,
-            coerceValue:OnNameCoerceValue);
-
-        
-
-        private static object OnNameCoerceValue(BindableObject bindable, object value)
-        {
-            //値の調整ロジックを記述
-            var name = (string)value;
-            if(name.Length > 10)
-            {
-                name = name.Substring(0,10);
-            }
-            return name;
-        }
-        private static bool OnNameValidationValue(BindableObject bindable, object value)
-        {
-            //バリデーションロジックを記述
-            var name = (string)value;
-
-            return !string.IsNullOrEmpty(name);
-        }
-
-        public static void OnNameChanged(BindableObject bindable,object oldValue, object newValue)
-        {
-            //変更前と変更後の値を使って何かをする
-
-        }
-
-        public string Name
-        {
-            get { return (string)this.GetValue(NameProperty);}
-            set { this.SetValue(NameProperty, value); }
-        }
-    }
     
     public partial class MainPage : ContentPage
     {
-        
-
         public MainPage()
         {
-        
-            InitializeComponent();
+            InitializeComponent();   
+        }
 
-            
+        private async void ButtonScrollTo_Clicked(object sender, EventArgs e)
+        {
+            await this.scrollView.ScrollToAsync(this.boxViewYellow, ScrollToPosition.Start, true);
+            Debug.WriteLine($"Scroll position is{this.scrollView.ScrollX},{this.scrollView.ScrollY}");
         }
     }
 }
