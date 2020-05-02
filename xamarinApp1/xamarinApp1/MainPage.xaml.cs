0b571599d1181b96310fad0665ccfc2cdecea2a4
dj8639rsm <dj8639rsm@gmail.com>
2020-05-02 17:50:26 +0900

    image　実装

--- xamarinApp1/xamarinApp1/MainPage.xaml.cs
+++ xamarinApp1/xamarinApp1/MainPage.xaml.cs
@@ -18,68 +18,7 @@ namespace xamarinApp1
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
 
-    public class Person
-    {
-        public string Name { get; set; }
-    }
-    public class BindableBase:INotifyPropertyChanged
-    {
-
-        protected BindableBase()
-        {
-            
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
-        {
-            if(object.Equals(field, value)) { return false; }
-
-            field = value;
-            this.OnPropertyChanged(propertyName);
-            return true;
-        }
-    }
-
-    public class MyPageViewModel : BindableBase
-    {
-        private string message;
-
-        public string Message
-        {
-            get {return this.message;}
-            set { this.SetProperty(ref this.message,value); }
-
-        }
-
-        public Command NowCommand { get; }
-
-        private bool canExcute;
-
-        public bool CanExcute
-        {
-            get { return this.canExcute; }
-            set
-            {
-                this.SetProperty(ref this.canExcute, value);
-                this.NowCommand.ChangeCanExecute();
-            }
-        }
-
-        public MyPageViewModel()
-        {
-            this.NowCommand = new Command(
-                x => this.Message = DateTime.Now.ToString((string)x),
-                _ => this.CanExcute);
-        }
-        
-    }
+    
     
     public partial class MainPage : ContentPage
     {
