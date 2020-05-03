3da8c61221a51517c4f1276c79b75edd527c16c6
dj8639rsm <dj8639rsm@gmail.com>
2020-05-03 09:31:26 +0900

    entry listView 実装

--- xamarinApp1/xamarinApp1/MainPage.xaml.cs
+++ xamarinApp1/xamarinApp1/MainPage.xaml.cs
@@ -19,50 +19,58 @@ namespace xamarinApp1
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
 
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class BindableBase : INotifyPropertyChanged
+    {
+
+        protected BindableBase()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool SerProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (object.Equals(field, value)) { return false; }
+
+            field = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+    public class MyPageViewModel : BindableBase
+    {
+        public ObservableCollection<Person> People { get; } = new ObservableCollection<Person>(
+            Enumerable.Range(1, 5).Select(x => new Person { Name = $"okazaki{x}" }));
+
+        public Command command { get; }
+
+        public MyPageViewModel()
+
+        {
+
+        }
+    }
+
     public partial class MainPage : ContentPage
     {
-        private ObservableCollection<Person> People { get; } = new ObservableCollection<Person>(
-            Enumerable.Range(1, 5).Select(x => new Person { Name = $"okazaki{x}" }));
 
         public MainPage()
         {
             InitializeComponent();
-
-            this.listView.ItemsSource = this.People;
         }
-        private void MenuItem1_Clicked(object sender, EventArgs e)
-        {
-            var selectedItem = ((MenuItem)sender).BindingContext as Person;
-            this.DisplayAlert(
-                "Info",
-                $"{selectedItem.Name}selocted",
-                "OK");
-        }
-        private void MenuItem2_Clicked(object sender, EventArgs e)
-        {
-            var selectedItem = ((MenuItem)sender).BindingContext as Person;
-            this.DisplayAlert(
-                "Info",
-                $"{selectedItem.Name}selected",
-                "OK");
-        }
-        private void MenuItem3_Clicked(object sender, EventArgs e)
-        {
-            var selectedItem = ((MenuItem)sender).BindingContext as Person;
-            this.DisplayAlert(
-                "Info",
-                $"{selectedItem.Name}selected",
-                "OK");
-        }
-
-
-
-
-
-        public class Person
-        {
-            public string Name { get; set; }
-        }
+       
     }
 }
 
